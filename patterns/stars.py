1ea06c67b1f941d3b2c28c0fab529107733c1e3c
pranay8873 <pranaypuvaati@gmail.com>
2026-07-24 00:49:18 +0530

    star pattern using for loops

---
+++ patterns/stars.py
@@ -0,0 +1,5 @@
+for i in range (4):
+    for j in range (4):
+        print("*",end="") #print s.ment by default consists of \n at the end of print fun ,end="" removes that \n
+                        #    ie;print=>print("xxxx",end="\n")
+    print("\n")
