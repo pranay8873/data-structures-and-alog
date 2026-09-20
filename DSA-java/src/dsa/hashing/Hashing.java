3bba05d1a27ccf922788cb785fb11595b419c36a
pranay8873 <pranaypuvaati@gmail.com>
2026-09-20 23:50:16 +0530

    DSA : created a hashed array which stores the repetence in a seperate array

--- DSA-java/src/dsa/hashing/Hashing.java
+++ DSA-java/src/dsa/hashing/Hashing.java
@@ -1,5 +1,8 @@
 package dsa.hashing;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Hashing {
 
     int count_n(int num,int[] array){
@@ -17,13 +20,41 @@ public class Hashing {
         return count;
     }
 
+    static void main() {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter size of array : ");
+        int size=sc.nextInt();
+        int[] arr=new int[size];
+        System.out.println("Enter Elements of array : ");
+        for (int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        int[] hash=new int[13];
+        for(int i=0;i<arr.length;i++){
+            hash[arr[i]]++;
+        }
+        System.out.println("Enter number of numbers to fetch : ");
+        int q=sc.nextInt();
+        while(q>0){
+            System.out.println("Number : ");
+            int number= sc.nextInt();
+            System.out.println("Times "+number+" Repeated : "+hash[number]);
+            q--;
+        }
 
+        System.out.println("Your array is : "+ Arrays.toString(arr));
+        System.out.println("Your hash array is : "+ Arrays.toString(hash));
 
-
-    void main(String[] args) {
-        int number=1;
-        int[] array={1,1,1,1,2,3,4,5};
-        System.out.println(" count : "+count_n(number,array));
 
     }
+
+
+
+//
+//    void main(String[] args) {
+//        int number=1;
+//        int[] array={1,1,1,1,2,3,4,5};
+//        System.out.println(" count : "+count_n(number,array));
+//
+//    }
 }
